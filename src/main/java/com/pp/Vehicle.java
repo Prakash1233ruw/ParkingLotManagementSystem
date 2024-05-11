b38d6a116bfed478eb7c0f04d9acd535c9d5334f
prakash <pr0018908@gmail.com>
2024-05-11 19:18:36 +0530

    Vehicle added

---
+++ src/main/java/com/pp/Vehicle.java
@@ -0,0 +1,12 @@
+package com.pp;
+
+public class Vehicle {
+  private  String type;
+  private  String registration;
+ private   String color;
+    public Vehicle(String type, String registration, String color) {
+        this.type = type;
+        this.registration = registration;
+        this.color = color;
+    }
+}
